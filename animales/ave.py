adc70461bd8556019f1bb83eeb94634c1f49966e
u1159 <ugo1@alu.ua.es>
2025-04-07 13:43:36 +0200

    animales

--- animales/ave.py
+++ animales/ave.py
@@ -2,7 +2,7 @@ from animales import Animal
 
 class Ave(Animal):
     """
-    Clase Ave, que hereda todos los métodos y atributos de la superclae animal.
+    Clase Ave, que hereda todos los métodos y atributos de la superclase animal.
     """
 
     def __init__(self, id_mascota, nombre, fecha_nacimiento, raza, sexo, n_identificacion,
