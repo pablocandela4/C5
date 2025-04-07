abbce7e2dd76dd877e1f3824c7cb6ea1042e1f1e
apg260 <apg260@alu.ua.es>
2025-04-07 13:51:10 +0200

    cambios

---
+++ alimentacion/alimentacion.py
@@ -0,0 +1,81 @@
+import csv
+from datetime import datetime
+from animales import Animal
+
+class Alimentacion:
+    def __init__(self, ):
+        self.historial = []
+
+    def mostrar(self):
+        for i, comida in enumerate(self.historial, 1):
+            print(f"{i}. {comida['tipo_comida']} ({comida['marca']}) | {comida['cantidad']}g | "
+                  f"Compra: {comida['fecha_compra'].date()} | Caduca: {comida['fecha_caducidad'].date()} | "
+                  f"{comida['coste']}€ en {comida['lugar_compra']}")
+
+
+    def añadir(self, tipo_comida, marca, cantidad, fecha_compra, fecha_caducidad, coste, lugar_compra):
+        alimento = {
+            "tipo_comida": tipo_comida,
+            "marca": marca,
+            "cantidad": cantidad,
+            "fecha_compra": datetime.strptime(fecha_compra, "%Y-%m-%d"),
+            "fecha_caducidad": datetime.strptime(fecha_caducidad, "%Y-%m-%d"),
+            "coste": coste,
+            "lugar_compra": lugar_compra
+        }
+    def guardar_csv(self, archivo):
+        with open(archivo, mode='w', newline='') as file:
+            campos = ["tipo_comida", "marca", "cantidad", "fecha_compra", "fecha_caducidad", "coste",
+                          "lugar_compra"]
+            writer = csv.DictWriter(file, fieldnames=campos)
+            writer.writeheader()
+            for comida in self.historial:
+                writer.writerow({
+                        "tipo_comida": comida["tipo_comida"],
+                        "marca": comida["marca"],
+                        "cantidad": comida["cantidad"],
+                        "fecha_compra": comida["fecha_compra"].strftime("%Y-%m-%d"),
+                        "fecha_caducidad": comida["fecha_caducidad"].strftime("%Y-%m-%d"),
+                        "coste": comida["coste"],
+                        "lugar_compra": comida["lugar_compra"]
+                    })
+
+    def cargar_csv(self, archivo):
+        try:
+            with open(archivo, mode='r') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    self.añadir(
+                            row["tipo_comida"],
+                            row["marca"],
+                            int(row["cantidad"]),
+                            row["fecha_compra"],
+                            row["fecha_caducidad"],
+                            float(row["coste"]),
+                            row["lugar_compra"]
+                        )
+        except FileNotFoundError:
+            print(f"Archivo {archivo} no encontrado. Empezando con historial vacío.")
+
+        # Sobrecarga de operadores
+    def __len__(self):
+        return len(self.historial)
+
+    def __getitem__(self, index):
+         return self.historial[index]
+
+    def __add__(self, otro):
+        nuevo = Alimentacion()
+        nuevo.historial = self.historial + otro.historial
+        return nuevo
+
+    def __iadd__(self, otro):
+        self.historial += otro.historial
+        return self
+
+
+
+
+
+
+
---
+++ alimentacion/historial.py
@@ -0,0 +1,12 @@
+from animales import Animal
+
+class Historial:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    def mostrar_historial_completo(nombre_mascota, alimentacion_obj):
+        print(f"\n📦 HISTORIAL DE ALIMENTACIÓN DE {nombre_mascota.upper()}")
+        print("-" * 60)
+        if len(alimentacion_obj) == 0:
+            print("Sin registros.")
+        else:
+            alimentacion_obj.mostrar()
