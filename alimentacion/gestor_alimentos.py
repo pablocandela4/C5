91f6e92b82a0b4540ddf39c744a2862e2311b1ea
apg260 <apg260@alu.ua.es>
2025-04-10 18:29:08 +0200

    cambio de historial a gestor_alimentos y añado funcion mostrar_gestion

--- alimentacion/gestor_alimentos.py
+++ alimentacion/gestor_alimentos.py
@@ -2,28 +2,28 @@ import csv
 import os
 from alimentacion import *
 
-historial = "historial.csv"
+gestor_alimentos = "gestor_alimentos.csv"
 
-class Historial:
+class Gestor:
     """Clase para gestionar el historial de catálogo y ventas de alimentos en un archivo CSV.
     Attributes
     ----------
     archivo : str
-        La ruta del archivo CSV donde se guarda el historial (por defecto "historial.csv").
+        La ruta del archivo CSV donde se guarda el historial y gestión (por defecto "gestor_alimentos.csv").
     """
-    def __init__(self, archivo=historial):
-        """Inicializa un objeto Historial con un archivo CSV.
+    def __init__(self, archivo=gestor_alimentos):
+        """Inicializa un objeto Gestor con un archivo CSV.
         Parameters
         ----------
         archivo : str, optional
-            La ruta del archivo CSV donde se almacenará el historial (por defecto "historial.csv").
+            La ruta del archivo CSV donde se almacenará el historial (por defecto "gestor_alimentos.csv").
         """
         self.archivo = archivo
         if not os.path.isfile(self.archivo):
             self.crear_archivo()
 
     def crear_archivo(self):
-        """Crea el archivo historial.csv con encabezados si no existe."""
+        """Crea el archivo gestor_alimentos.csv con encabezados si no existe."""
         with open(self.archivo, mode='w', newline='') as archivo:
             archivo.write("--- CATÁLOGO DE ALIMENTOS ---\n")
             writer = csv.writer(archivo)
@@ -33,7 +33,7 @@ class Historial:
         print(" Archivo historial.csv creado correctamente.")
 
     def guardar_alimentos(self, catalogo):
-        """Guarda el catálogo de alimentos en el archivo historial.csv.
+        """Guarda el catálogo de alimentos en el archivo gestor_alimentos.csv.
         Parameters
         ----------
         catalogo : list[Alimento]
@@ -51,7 +51,7 @@ class Historial:
                ])
 
     def guardar_ventas(self, carrito):
-        """Agrega productos vendidos al final del historial.csv bajo el apartado de ventas.
+        """Agrega productos vendidos al final del gestor_alimentos.csv bajo el apartado de ventas.
         Parameters
         ----------
         carrito : list[Alimento]
@@ -67,3 +67,13 @@ class Historial:
                     producto.fecha_caducidad.date(),
                     f"{producto.coste:.2f}"
                 ])
+
+    def mostrar_gestion(self):
+        """Muestra el contenido de gestor_alimentos.csv."""
+        if not os.path.isfile(self.archivo):
+            print("El archivo no existe.")
+            return
+
+        with open(self.archivo, mode='r', encoding='utf-8') as archivo:
+            contenido = archivo.read()
+            print(contenido)
